04e5a40766cbdd757e20f3201187526d8ffe81d1
BYIRINGIRO-Samuel <byiringirosam08@gmail.com>
2026-04-12 20:20:11 +0100

    Replace Lorem Ipsum with real FoodFlow content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
     <div className='footer' id='footer'>
         <div className="footer-content">
             <div className="footer-content-left">
-                <img src={assets.logo} alt="" />
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, tenetur esse earum magni eum neque aut eius deleniti voluptatum et impedit dolor ut dolorum incidunt ad minima vero maxime tempora!</p>
+                <h3 className="footer-logo">FoodFlow</h3>
+                <p>Your trusted food delivery partner bringing delicious meals from your favorite restaurants right to your doorstep. Fast, reliable, and always fresh.</p>
                 <div className="footer-social-icons">
                     <img src={assets.facebook_icon} alt="" />
                     <img src={assets.twitter_icon} alt="" />
@@ -18,16 +18,17 @@ const Footer = () => {
                 <h2>COMPANY</h2>
                 <ul>
                     <li>Home</li>
-                    <li>About us</li>
+                    <li>About Us</li>
                     <li>Delivery</li>
-                    <li>Privacy policy</li>
+                    <li>Privacy Policy</li>
                 </ul>
             </div>
             <div className="footer-content-right">
                <h2>GET IN TOUCH</h2>
                <ul>
-                <li>+250790395120</li>
-                <li>contact@foodflow.com</li>
+                <li>+1-800-FOODFLOW</li>
+                <li>support@foodflow.com</li>
+                <li>24/7 Customer Support</li>
                </ul>
             </div>
         </div>
